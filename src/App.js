d8d3192d3ef2700d85ce2f7403ca87f7c9b25589
Thaiza12 <87837910+Thaiza12@users.noreply.github.com>
2021-11-21 10:03:29 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Dados from "./Dados.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Olá Thaiza - Recife PE</h1>
+      <Dados/>
+    </div>
+  );
+}
---
+++ src/Dados.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Dados(props) {
+  return (
+    <div>
+      <p> Estou escrevendo essa carta por causa da </p>
+      <p>atividade que os alunos vão fazer, okay.</p>
+      <p>Não desiste, continue fazendo as atividades, treinando e praticando. </p>
+      <p>O Céu  é o limite!</p>
+ 
+    </div>
+  );
+}
